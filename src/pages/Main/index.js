68bcc4b829dc62f8928f5f9fffd48b202da33a71
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-04-21 17:23:58 -0300

    Installed styled-components.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Main() {
-  return (
-    <View>
-      <Text>Main</Text>
-    </View>
-  );
+  return <Container />;
 }
