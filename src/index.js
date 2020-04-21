750ae074b992a4d27198cf1738763a0a5c1d0a2b
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-04-21 17:09:11 -0300

    Routing basic structure.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hello World!</Text>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from '../pages/Main';
+import Users from '../pages/Users';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Main"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerBackTitleVisible: false,
+        headerStyle: { backgroundColor: '#7159c1' },
+        headerTintColor: '#fff',
+      }}
+    >
+      <Stack.Screen name="Main" component={Main} options={{ title: 'Home' }} />
+      <Stack.Screen name="Users" component={Users} />
+    </Stack.Navigator>
+  );
+}
