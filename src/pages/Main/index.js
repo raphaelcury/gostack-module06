ffaecf6561b5cdc2245f3ebce7fb4759e790455b
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-04-22 19:55:17 -0300

    Added state for input and button handling.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#fff" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+  };
+
+  handleAddButton = () => {
+    console.tron.log(this.state.newUser);
+  };
+
+  render() {
+    const { newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+          />
+          <SubmitButton onPress={this.handleAddButton}>
+            <Icon name="add" size={20} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
