874ab752ad1ee7e7295f456bb0d1eb9a0f0f6ecc
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-04-25 13:38:11 -0300

    Transformed Users function into Class.

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
 // import { Container } from './styles';
 
-export default function Users() {
-  return (
-    <View>
-      <Text>Users</Text>
-    </View>
-  );
+export default class Users extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({ navigate: PropTypes.func }).isRequired,
+  };
+
+  render() {
+    const { route } = this.props;
+    console.tron.log(route.params);
+    return (
+      <View>
+        <Text>Users</Text>
+      </View>
+    );
+  }
 }
